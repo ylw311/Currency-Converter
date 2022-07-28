e7c2c5bda79e72e090cdf5e6c2b344e0ef1851c2
Linda Wang <61102300+ylw311@users.noreply.github.com>
2022-07-28 04:54:35 +0000

    fixed issue of not displaying output when input 0 + rounded output to two decimal places

--- script.js
+++ script.js
@@ -9,9 +9,9 @@ fetch("https://api.frankfurter.app/currencies").then((data) => data.json()).then
 })
 
 // display currency list in select option box, from api link
-function display(data){
+function display(data) {
   const entries = Object.entries(data);
-  for(var i=0; i < entries.length; i++) {
+  for (var i = 0; i < entries.length; i++) {
     select[0].innerHTML += `<option value="${entries[i][0]}"> ${entries[i][0]} </option>`;
     select[1].innerHTML += `<option value="${entries[i][0]}"> ${entries[i][0]} </option>`;
   }
@@ -32,30 +32,36 @@ document.addEventListener("keyup", function(event) {
 */
 
 //auto convert every second
-var intervalId = window.setInterval(function(){
+var intervalId = window.setInterval(function() {
   check()
 }, 100);
 
-function check(){
+//checks if there are values in input box to be converted
+function check() {
   let currency1 = select[0].value;
   let currency2 = select[1].value
 
   let value = input.value;
+  console.log(value)
 
-  if(currency1 != currency2){
+  if (value === '0') {
+    output.value = value;
+  } else if (currency1 != currency2) {
     convert(value, currency1, currency2);
   } else {
+    // if currency is same, simply output the same result
     output.value = value;
   }
 }
 
+// converts and outputs the result
 function convert(value, currency1, currency2) {
   const host = "api.frankfurter.app";
   fetch(
     `https://${host}/latest?amount=${value}&from=${currency1}&to=${currency2}`
   ).then((val) => val.json()).then((val) => {
-      output.value = Object.values(val.rates)[0];
-    });
+    output.value = Math.round((Object.values(val.rates)[0] + Number.EPSILON) * 100) / 100;
+  });
 }
 
 
