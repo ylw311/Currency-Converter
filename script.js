4e2b0c3440416b89e92771b253fe2be96b3c0170
Linda Wang <61102300+ylw311@users.noreply.github.com>
2022-07-28 03:50:11 +0000

    done design (css)

---
+++ script.js
@@ -0,0 +1,10 @@
+const select = document.querySelectorAll(".currecny");
+
+const input = document.getElementById("input");
+const output = document.getElementById("output");
+const button = document.getElementById("button");
+
+console.log(select);
+console.log(button);
+console.log(input);
+console.log(output);
